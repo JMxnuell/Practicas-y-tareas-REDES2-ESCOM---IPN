b2c3667019ef3c22d1e1a9b52fef9e0217deb88e
Manuel <jsuarez1804@alumno.ipn.mx>
2022-09-17 18:48:44 -0500

    Corrigiendo errores servidor cliente

--- Tarea1_SBJM/src/main/java/escom/ipn/tarea1_sbjm/servidor.java
+++ Tarea1_SBJM/src/main/java/escom/ipn/tarea1_sbjm/servidor.java
@@ -15,28 +15,40 @@ public class servidor {
           s.setReuseAddress(true);
           System.out.println("Servidor iniciado...");
           
-          Socket cl = s.accept();
-          System.out.println("Cliente conectado desde "+cl.getInetAddress()+":"+cl.getPort());
-          DataInputStream dis = new DataInputStream(cl.getInputStream());
-          
-          int dificultad = dis.readInt();
-          
-          System.out.println("Se recibio la dificultad: " + dificultad);
-          palabra pGenerada = new palabra(dificultad);
-          ObjectOutputStream oos = new ObjectOutputStream(cl.getOutputStream());
-          oos.writeObject(pGenerada.getSegmentos()); // mandamos el total de segmentos (ya que puede que se tengan espacios)
-          System.out.println("Se mandaron los segmentos de la palabra");
-          BufferedReader br = new BufferedReader(new InputStreamReader(cl.getInputStream()));
           for(;;){
+            System.out.println("Esperando cliente");
+            Socket cl = s.accept();
+            System.out.println("Cliente conectado desde "+cl.getInetAddress()+":"+cl.getPort());
+            DataInputStream dis = new DataInputStream(cl.getInputStream());
+
+            int dificultad = dis.readInt();
+
+            System.out.println("Se recibio la dificultad: " + dificultad);
+            palabra pGenerada = new palabra(dificultad);
+            ObjectOutputStream oos = new ObjectOutputStream(cl.getOutputStream());
+            oos.writeObject(pGenerada.getSegmentos()); // mandamos el total de segmentos (ya que puede que se tengan espacios)
+            System.out.println("Se mandaron los segmentos de la palabra");
+            BufferedReader br = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+              for(;;){
+              System.out.println("Esperando respuesta del cliente");
               String lRecibida = br.readLine();
               System.out.println("recibido: " + lRecibida);
-              if(lRecibida == "FINISHED")
-                  break;
-              oos.writeObject(pGenerada.encontrados(lRecibida)); //mandamos los indices en los que se encontraron la letra
+              if(lRecibida.compareToIgnoreCase("FINISH") == 0){
+                  System.out.println("JUEGO FINALIZADO");
+                  // mandamos la palabra que se debió adivinar
+                  PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+                  pw.println(pGenerada.getGenerada()); 
+                  pw.close();
+                  dis.close();
+                  oos.close(); 
+                  cl.close();
+                  break; // El juego a finalizado
+              }else{
+                  System.out.println("Mandado respuesta al cliente");
+                  oos.writeObject(pGenerada.encontrados(lRecibida));
+              } //mandamos los indices en los que se encontraron la letra
+              }
           }
-          dis.close();
-          oos.close(); 
-          cl.close();
       }catch(Exception e){
           e.printStackTrace();
       }  
